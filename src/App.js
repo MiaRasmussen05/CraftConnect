37b5f8708fb4384abf8f96da3a17cba21fde8f9a
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-18 01:15:40 +0000

    Add signout event handler with updates for the navbar and missing route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,57 @@
-import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import logo from '../assets/logo.png'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active} to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink className={styles.NavLink}
+        activeClassName={styles.Active} to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active} to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink to="/signin" className={styles.NavLink} 
@@ -26,19 +69,22 @@ const NavBar = () => {
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
           <NavLink to="/">
-            <Navbar.Brand><img src={logo} alt='logo' height='75' />Craft|Hub</Navbar.Brand>
+            <Navbar.Brand>
+              <img src={logo} alt='logo' height='75' />Craft|Hub
+            </Navbar.Brand>
           </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-right">
-                  <NavLink to="/" exact activeClassName={styles.Active}>
-                    <i className='fas fa-home'></i>Home
-                  </NavLink>
+          {currentUser && addPostIcon}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto text-right">
+                <NavLink to="/" exact activeClassName={styles.Active}>
+                  <i className='fas fa-home'></i>Home
+                </NavLink>
 
-                  {currentUser ? loggedInIcons : loggedOutIcons}
+                {currentUser ? loggedInIcons : loggedOutIcons}
 
-                </Nav>
-            </Navbar.Collapse>
+              </Nav>
+          </Navbar.Collapse>
         </Container>
     </Navbar>
   )
