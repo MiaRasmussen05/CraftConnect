02bd435c9a951bee48e49f0b4cc69ac9bcae5f31
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-18 14:33:22 +0000

    Create the post component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Post from "./Post";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
@@ -32,7 +33,7 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
