74c53bc894bf49fb8f83ac4e4bff0353ebb9aae6
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-19 23:17:41 +0000

    Create the display for the profile component

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
+import Profile from "./Profile";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = ({ mobile }) => {
@@ -44,12 +45,12 @@ const PopularProfiles = ({ mobile }) => {
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
-                <p key={profile.id}>{profile.owner}</p>
+                <Profile key={profile.id} profile={profile} mobile />
               ))}
             </div>
           ) : (
             popularProfiles.results.map((profile) => (
-              <p key={profile.id}>{profile.owner}</p>
+              <Profile key={profile.id} profile={profile} />
             ))
           )}
         </>
