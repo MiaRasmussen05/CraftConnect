a0d77c1fcf722807190bd00262410dfc4b3913a6
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-24 17:36:58 +0000

    Add other half of form and set up to api

--- src/pages/events/EventCreateForm.js
+++ src/pages/events/EventCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import { Form, Button, Row, Col, Container, Image } from "react-bootstrap";
 
@@ -9,19 +9,32 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
 function EventCreateForm() {
 
-  const [postData, setPostData] = useState({
+  const [setErrors] = useState({});
+
+  const [eventData, setEventData] = useState({
     name: "",
     description: "",
     start_date: "",
+    end_date: "",
+    location: "",
+    website_link: "",
+    cost: "",
     image: ""
   });
-  const { name, description, start_date, image } = postData;
+  const { name, description, start_date, end_date,
+      location, website_link, cost, image } = eventData;
+    
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
-    setPostData({
-      ...postData,
+    setEventData({
+      ...eventData,
       [event.target.name]: event.target.value,
     });
   };
@@ -29,12 +42,35 @@ function EventCreateForm() {
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
-      setPostData({
-          ...postData,
+      setEventData({
+          ...eventData,
           image: URL.createObjectURL(event.target.files[0]),
       });
     } 
-  } 
+  };
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("end_date", end_date);
+    formData.append("location", location);
+    formData.append("website_link", website_link);
+    formData.append("cost", cost);
+    formData.append("image", imageInput.current.files[0]);
+
+    try {
+      const { data } = await axiosReq.post("/events/", formData);
+      history.push(`/events/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
 
   const textFields = (
     <div className="text-center p-3">
@@ -59,10 +95,39 @@ function EventCreateForm() {
           value={start_date} onChange={handleChange}
         />
       </Form.Group>
+      <Form.Group>
+        <Form.Label>End date</Form.Label>
+        <Form.Control
+          type="datetime-local" rows={6} name="end_date"
+          value={end_date} onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+          type="text" name="location"
+          value={location} onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Website</Form.Label>
+        <Form.Control
+          type="url" name="website_link"
+          value={website_link} onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Cost</Form.Label>
+        <Form.Control
+          type="number" name="cost"
+          value={cost} onChange={handleChange}
+          min="0"
+        />
+      </Form.Group>
 
       <Button
-        className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit"
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -73,7 +138,7 @@ function EventCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col md={7} lg={8} className="d-none d-md-block p-0 p-md-2">
           <Container className={appStyles.Content}>{textFields}</Container>
