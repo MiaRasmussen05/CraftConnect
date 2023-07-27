0d98bf0c6c1b39b8e948d07a26cf3f648d558721
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-27 01:42:44 +0000

    Add start and end date and location to the form text fields with errors for event edit form

--- src/pages/events/EventEditForm.js
+++ src/pages/events/EventEditForm.js
@@ -8,21 +8,25 @@ function EventEditForm() {
   const [errors] = useState({});
 
   const [postData] = useState({
-    title: "",
+    name: "",
     description: "",
+    start_date_time: "",
+    end_date_time: "",
+    location: "Unknown",
   });
-  const { title, description } = postData;
+  const { name, description, start_date_time, end_date_time,
+    location } = postData;
 
   const textFields = (
     <div className="text-center p-3">
       <Form.Group>
       <Form.Label>Title</Form.Label>
       <Form.Control
-        type="text" name="title"
-        value={title}
+        type="text" name="name"
+        value={name}
       />
       </Form.Group>
-      {errors?.title?.map((message, idx) => (
+      {errors?.name?.map((message, idx) => (
       <Alert variant="warning" key={idx}>
         {message}
       </Alert>
@@ -36,6 +40,45 @@ function EventEditForm() {
         />
       </Form.Group>
       {errors?.description?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Start date</Form.Label>
+        <Form.Control
+          type="datetime-local" rows={6} name="start_date_time"
+          value={start_date_time}
+        />
+      </Form.Group>
+      {errors?.start_date_time?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>End date</Form.Label>
+        <Form.Control
+          type="datetime-local" rows={6} name="end_date_time"
+          value={end_date_time}
+        />
+      </Form.Group>
+      {errors?.end_date_time?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+<Form.Group>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+          type="text" name="location"
+          value={location}
+        />
+      </Form.Group>
+      {errors?.location?.map((message, idx) => (
         <Alert variant="warning" key={idx}>
           {message}
         </Alert>
