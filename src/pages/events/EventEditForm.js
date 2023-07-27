7123380c731c05e49ff7c0a48b562de9fc494b4b
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-27 02:05:47 +0000

    Add the cover image form group to the form with errors for event edit form

--- src/pages/events/EventEditForm.js
+++ src/pages/events/EventEditForm.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-import { Form, Row, Col, Container, Alert, Button }from "react-bootstrap";
+import { Form, Row, Col, Container, Alert, Button, Image }from "react-bootstrap";
+import styles from "../../styles/PostCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+import upload from "../../assets/upload.png";
+import Asset from "../../components/Asset";
 
 
 function EventEditForm() {
   
   const [errors] = useState({});
 
-  const [postData] = useState({
+  const [eventData] = useState({
     name: "",
     description: "",
     start_date_time: "",
@@ -16,9 +21,12 @@ function EventEditForm() {
     location: "Unknown",
     website_link: "",
     cost: 0,
+    cover_image: ""
   });
   const { name, description, start_date_time, end_date_time,
-    location, website_link, cost } = postData;
+    location, website_link, cost, cover_image } = eventData;
+
+  const imageInput = useRef(null);
 
   const textFields = (
     <div className="text-center p-3">
@@ -129,6 +137,49 @@ function EventEditForm() {
       <Row>
         <Col md={7} lg={8} className="d-none d-md-block p-0 p-md-2">
           <Container>{textFields}</Container>
+        </Col>
+        <Col className="py-2 p-0 p-md-2" md={5} lg={4}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center p-md-5`}
+          >
+            <Form.Group className="text-center">
+              {cover_image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={cover_image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the cover image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center" 
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={upload} message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload" accept="image/*" ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className="d-md-none">{textFields}</div>
+          </Container>
         </Col>
       </Row>
     </Form>
