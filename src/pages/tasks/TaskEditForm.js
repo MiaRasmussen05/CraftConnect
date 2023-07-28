319d3fe090009415c20b9e93cb06c5700176e10c
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-28 19:24:59 +0000

    Create the edit form boiler plate for the tasks

---
+++ src/pages/tasks/TaskEditForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { axiosReq } from "../../api/axiosDefaults";
+import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function TaskEditForm(props) {
+  const { id, title, setShowEditForm, setTasks } = props;
+
+  const [formTitle, setFormTitle] = useState(title);
+
+  const handleChange = (event) => {
+    setFormTitle(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if (formTitle.trim() === title) {
+        setShowEditForm(false);
+        return;
+      }
+
+      await axiosReq.put(`/tasks/${id}/`, {
+        title: formTitle.trim(),
+      });
+
+      setTasks((prevTasks) => ({
+        ...prevTasks,
+        results: prevTasks.results.map((task) =>
+          task.id === id
+            ? {
+                ...task,
+                title: formTitle.trim(),
+                updated_at: "now",
+              }
+            : task
+        ),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          value={formTitle}
+          onChange={handleChange}
+          rows={2}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={styles.Button}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          Cancel
+        </button>
+        <button
+          className={styles.Button}
+          disabled={!formTitle.trim() || formTitle.trim() === title}
+          type="submit"
+        >
+          Save
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default TaskEditForm;
