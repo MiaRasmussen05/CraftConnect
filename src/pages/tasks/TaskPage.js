a488b69e73c7e72e23135646dc9e886b224bdb1b
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-28 19:26:53 +0000

    Set up the tasks component on thhe task page

--- src/pages/tasks/TaskPage.js
+++ src/pages/tasks/TaskPage.js
@@ -13,12 +13,16 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import IdeasCreateForm from "../tasks/IdeasCreateForm";
 import TaskCreateForm from "../tasks/TaskCreateForm";
 import Idea from "../tasks/Idea";
+import Task from "../tasks/Task";
 
 function TaskPage() {
   const { id } = useParams();
 
   const currentUser = useCurrentUser();
   const [ideas, setIdeas] = useState({ results: [] });
+  const [tasks, setTasks] = useState({ results: [] });
+  const [setTitle] = useState("");
+
 
   useEffect(() => {
     const fetchIdeas = async () => {
@@ -33,6 +37,20 @@ function TaskPage() {
     };
 
     fetchIdeas();
+  }, [id]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axiosReq.get(`/tasks/`);
+        const tasksData = response.data;
+        setTasks(tasksData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
+    fetchTasks();
   }, [id]);
 
   return (
@@ -67,11 +85,37 @@ function TaskPage() {
           )}
         </Container>
       </Col>
-      <Col className="py-3 p-lg-3">
+      <Col className="py-3 p-lg-3" lg={5}>
         <h2 className={`${taskStyles.Header} text-center mt-5`}>Todo Task</h2>
         <Container className={`${appStyles.Content} p-4`}>
-          <TaskCreateForm />
+          <TaskCreateForm setTasks={setTasks} setTitle={setTitle} tasks={tasks} />
         </Container>
+      </Col>
+      <Col>
+      {currentUser ? (
+            <br />
+          ) : tasks.results.length ? (
+            "Tasks"
+          ) : null}
+          {tasks.results.length ? (
+            <InfiniteScroll
+              children={tasks.results.map((tasks) => (
+                <Container>
+                  <Task key={tasks.id} {...tasks} setTasks={setTasks} />
+                </Container>
+              ))}
+              dataLength={tasks.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!tasks.next}
+              next={() => fetchMoreData(tasks, setTasks)}
+            />
+          ) : currentUser ? (
+            <span>No tasks... yet</span>
+          ) : (
+            <Container>
+              <Asset spinner />
+            </Container>
+          )}
       </Col>
     </Row>
   );
